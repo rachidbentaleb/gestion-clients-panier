437851cdc1b551c0f7ee7f2b30030f965ddcc98d
Rachid <rachidbentaleb2002@gmail.com>
2025-01-13 18:30:00 +0100

    Updated README and some small details

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,7 +13,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <Link to="/" className="navbar-brand">
-                    Gestion Clients
+                    Gestion Livres
                 </Link>
                 <button
                     className="navbar-toggler"
